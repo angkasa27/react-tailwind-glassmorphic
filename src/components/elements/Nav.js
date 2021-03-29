c58899f2ad24498e5944daf5021370e90e0cc09f
Dimas Angkasa Nurindra <dimas_angkasa_27rpl@student.smktelkom-mlg.sch.id>
2021-03-29 21:28:45 +0700

    Add Project Page

---
+++ src/components/elements/Nav.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default function Nav({ className }) {
+  return (
+    <header
+      className={
+        'bg-window sm:px-5 py-3 md:absolute right-1/4 left-5/100 bg-glass z-50 transition-all duration-500 ease-in-out animate-itemUp md:animate-itemRight ' +
+        className
+      }
+    >
+      <div className="flex flex-col sm:flex-row justify-center sm:justify-between items-center">
+        <NavLink
+          to="/"
+          className="cursor-pointer text-yellow-300 hover:text-yellow-400 transition-default text-2xl font-black w-full sm:w-auto text-center border-b sm:border-0"
+        >
+          angkasa27
+        </NavLink>
+        <div className="mt-2 sm:mt-0">
+          <NavLink
+            to="/about"
+            className="transition-default nav-link mr-8"
+            activeClassName="bg-yellow-400 text-white"
+          >
+            About
+          </NavLink>
+          <NavLink
+            to="/project"
+            className="transition-default nav-link mr-8"
+            activeClassName="bg-yellow-400 text-white"
+          >
+            Project
+          </NavLink>
+          <NavLink
+            to="/contact"
+            className="transition-default nav-link"
+            activeClassName="bg-yellow-400 text-white"
+          >
+            Contact
+          </NavLink>
+        </div>
+      </div>
+    </header>
+  );
+}
---
+++ src/pages/Project/Project.js
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from 'react';
+
+export default function Project() {
+  return (
+    <div>
+      <Main>
+        {Data.map((data, idx) => (
+          <Windows data={data} idx={idx} />
+        ))}
+      </Main>
+    </div>
+  );
+}
+
+export function Main({ children }) {
+  const [classes, setClasses] = useState('invisible md:visible');
+
+  useEffect(() => {
+    setTimeout(() => {
+      setClasses(' animate-itemUp md:animate-none');
+    }, 100);
+  }, []);
+
+  return (
+    <main
+      className={
+        'bg-window md:absolute top-1/10 bottom-1/10 left-1/10 right-1/10 bg-glass z-10 mt-5 md:mt-0 transition-default overflow-y-hidden pb-12 ' +
+        classes
+      }
+    >
+      <Header name="project" />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 w-full h-full p-5 overflow-y-auto">
+        {children}
+      </div>
+    </main>
+  );
+}
+
+export function Header({ name }) {
+  return (
+    <div
+      className={
+        'flex justify-between px-3 border-b ' + (name ? 'py-1' : 'py-3')
+      }
+    >
+      <p className="font-mono font-bold">{name}</p>
+      <div className="flex items-center w-12 justify-between">
+        <div className="w-3 h-3 rounded-full bg-yellow-400" />
+        <div className="w-3 h-3 rounded-full bg-yellow-500" />
+        <div className="w-3 h-3 rounded-full bg-red-600" />
+      </div>
+    </div>
+  );
+}
+
+export function Windows({ data, idx }) {
+  const [classes, setClasses] = useState(' invisible');
+
+  useEffect(() => {
+    setTimeout(() => {
+      setClasses(' animate-scale');
+    }, idx * 100 + 100);
+  }, []);
+
+  return (
+    <span
+      className={
+        ' hover:bg-gray-800 hover:bg-opacity-10 rounded-xl p-2 ' + classes
+      }
+    >
+      <div className="px-20 py-8 h-40 flex flex-col items-center bg-white rounded-lg ">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 354.8 400.44"
+          className="fill-current h-16 text-gray-800"
+        >
+          <path
+            class="cls-1"
+            d="M0,121.15V307.3l58.63,33.85V290.37l44,25.39v50.78l58.63,33.85V214.25ZM102.6,265l-44-25.37V205.78l44,25.39Z"
+          />
+          <polygon
+            class="cls-1"
+            points="16.16 93.19 177.38 0 221.36 25.48 104.1 93.19 118.76 101.65 236.01 33.94 338.61 93.19 177.38 186.28 133.41 160.89 250.75 93.14 236.01 84.72 118.76 152.43 16.16 93.19"
+          />
+          <path
+            class="cls-1"
+            d="M193.51,214.23V400.44l58.65-33.87V315.78l44-25.4v50.79l58.65-33.86V121.1Zm102.64,25.35-44,25.4V231.16l44-25.4Z"
+          />
+        </svg>
+      </div>
+      <p className="font-mono text-sm mt-2 text-center">{data.name}</p>
+    </span>
+  );
+}
+
+export const Data = [
+  { name: 'ASA Logo.png' },
+  { name: 'ASA Logo.png' },
+  { name: 'ASA Logo.png' },
+  { name: 'ASA Logo.png' },
+  { name: 'ASA Logo.png' },
+  { name: 'ASA Logo.png' },
+  { name: 'ASA Logo.png' },
+  { name: 'ASA Logo.png' },
+];
